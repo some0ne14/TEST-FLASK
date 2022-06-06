4a2266a08cc45cd0de4bc8e54f49699dc0412190
some0ne14 <96661853+some0ne14@users.noreply.github.com>
2022-06-06 15:41:25 -0600

    Add files via upload

---
+++ flask-practice/application.py
@@ -0,0 +1,11 @@
+from math-Ibex import app as application  # application for AWS
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-flask.html
+
+#__name__ will be main if this is ran directly with Python
+# but not if imported to another module
+#print("__name__ =", __name__)
+# if __name__ == '__main__':
+#    app.run(debug=True)
+#    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+# else:
+#    print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
